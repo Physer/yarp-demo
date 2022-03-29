4d533c8e390fc9fb04aa14934d536d2624283ff3
Alex Schouls <alex_schouls@live.com>
2022-03-29 10:27:45 +0200

    Added API proxy

--- src/Yarp.Forwarder/Program.cs
+++ src/Yarp.Forwarder/Program.cs
@@ -13,23 +13,36 @@ var httpClient = new HttpMessageInvoker(new SocketsHttpHandler()
     UseCookies = false
 });
 var forwarder = app.Services.GetRequiredService<IHttpForwarder>();
-var transformer = new SimpleTransformer();
+var simpleTransformer = new SimpleTransformer();
+var defaultTransformer = HttpTransformer.Default;
 var requestConfig = new ForwarderRequestConfig { ActivityTimeout = TimeSpan.FromSeconds(100) };
 
 app.UseRouting();
 app.UseEndpoints(endpoints =>
 {
+    // Landing
     endpoints.Map("/", () => "Please enter a valid route");
+
+    // Google
     endpoints.Map("/google/{**catch-all}", async httpContext =>
     {
-        var error = await forwarder.SendAsync(httpContext, "http://www.google.com/",
-            httpClient, requestConfig, transformer);
+        var error = await forwarder.SendAsync(httpContext, "http://www.google.com/", httpClient, requestConfig, simpleTransformer);
+        if (error == ForwarderError.None)
+            return;
 
-        if (error != ForwarderError.None)
-        {
-            var errorFeature = httpContext.GetForwarderErrorFeature();
-            var exception = errorFeature?.Exception;
-        }
+        var errorFeature = httpContext.GetForwarderErrorFeature();
+        var exception = errorFeature?.Exception;
+    });
+
+    // API
+    endpoints.Map("/{**catch-all}", async httpContext =>
+    {
+        var error = await forwarder.SendAsync(httpContext, "https://localhost:7013/", httpClient, requestConfig, defaultTransformer);
+        if (error == ForwarderError.None)
+            return;
+
+        var errorFeature = httpContext.GetForwarderErrorFeature();
+        var exception = errorFeature?.Exception;
     });
 });
 
