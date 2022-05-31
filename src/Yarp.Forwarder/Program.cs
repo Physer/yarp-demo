77e905e64e8bb5316fed56dec1312a41f32fed4b
Alex Schouls <alex.schouls@valtech.com>
2022-05-31 11:10:21 +0200

    Added comments

--- src/Yarp.Forwarder/Program.cs
+++ src/Yarp.Forwarder/Program.cs
@@ -3,8 +3,13 @@ using Yarp.Forwarder;
 using Yarp.ReverseProxy.Forwarder;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Adding YARP's HTTP Forwarder
 builder.Services.AddHttpForwarder();
+
 var app = builder.Build();
+
+// Setting up an HTTP Message Invoker for use in the HTTP Forwarder
 var httpClient = new HttpMessageInvoker(new SocketsHttpHandler()
 {
     UseProxy = false,
@@ -12,18 +17,25 @@ var httpClient = new HttpMessageInvoker(new SocketsHttpHandler()
     AutomaticDecompression = DecompressionMethods.None,
     UseCookies = false
 });
+
+// Getting the forwarder from the IoC container
 var forwarder = app.Services.GetRequiredService<IHttpForwarder>();
+
+// Creating the necessary objects for our YARP configuration
 var simpleTransformer = new SimpleTransformer();
 var defaultTransformer = HttpTransformer.Default;
 var requestConfig = new ForwarderRequestConfig { ActivityTimeout = TimeSpan.FromSeconds(100) };
 
 app.UseRouting();
+
+// Mapping three different endpoints to showcase the handling of YARP
 app.UseEndpoints(endpoints =>
 {
-    // Landing
+    // The landing endpoint, simply showing a bit of text for the user
     endpoints.Map("/", () => "Please enter a valid route");
 
-    // Google
+    // Everything that has /google in the query string will redirect to the google.com domain
+    // Using a custom transformer object
     endpoints.Map("/google/{**catch-all}", async httpContext =>
     {
         var error = await forwarder.SendAsync(httpContext, "http://www.google.com/", httpClient, requestConfig, simpleTransformer);
@@ -34,7 +46,8 @@ app.UseEndpoints(endpoints =>
         var exception = errorFeature?.Exception;
     });
 
-    // API
+    // All other requests will be mapped to our API endpoint, which may or may not be able to handle the request
+    // Using the default HTTP transformer rules bundled with YARP
     endpoints.Map("/{**catch-all}", async httpContext =>
     {
         var error = await forwarder.SendAsync(httpContext, "https://localhost:7013/", httpClient, requestConfig, defaultTransformer);
--- src/Yarp.Forwarder/SimpleTransformer.cs
+++ src/Yarp.Forwarder/SimpleTransformer.cs
@@ -6,8 +6,12 @@ public class SimpleTransformer : HttpTransformer
 {
     public override async ValueTask TransformRequestAsync(HttpContext httpContext, HttpRequestMessage proxyRequest, string destinationPrefix)
     {
+        // Using the base call to copy over all values of the original request (including headers)
         await base.TransformRequestAsync(httpContext, proxyRequest, destinationPrefix);
+
+        // Customising the request so that our URI will actually be what we pass in
         proxyRequest.RequestUri = RequestUtilities.MakeDestinationAddress(destinationPrefix, PathString.Empty, QueryString.Empty);
+        // Set the Host header to null so we get redirected to Google
         proxyRequest.Headers.Host = null;
     }
 }
