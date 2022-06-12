211b68d73a492ee7222d548aab18291fd6f14ca9
Alex Schouls <alex_schouls@live.com>
2022-06-12 21:48:19 +0200

    Added serverless project (Functions) and started on Auth0/AuthN

--- src/Yarp.Auth/Program.cs
+++ src/Yarp.Auth/Program.cs
@@ -1,5 +1,25 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.MicrosoftAccount;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var configuration = builder.Configuration;
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = MicrosoftAccountDefaults.AuthenticationScheme;
+    })
+    .AddCookie()
+    .AddMicrosoftAccount(options =>
+    {
+        options.ClientId = configuration["ClientId"];
+        options.ClientSecret = configuration["ClientSecret"];
+    });
+
 var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapReverseProxy();
 app.Run();
